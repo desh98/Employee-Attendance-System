620ca1a90c8ca772f0743dd41145df3a0d530295
desh98 <73052432+desh98@users.noreply.github.com>
2024-03-12 17:56:06 +0530

    BottomNavBar => Updated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,12 @@
+import 'package:curved_labeled_navigation_bar/curved_navigation_bar.dart';
+import 'package:curved_labeled_navigation_bar/curved_navigation_bar_item.dart';
 import 'package:flutter/material.dart';
+import 'package:ientrada_new/constants/color.dart';
+import 'package:ientrada_new/screens/employee_screen.dart';
+import 'package:ientrada_new/screens/entrance_screen.dart';
+import 'package:ientrada_new/screens/exit_screen.dart';
 import 'package:ientrada_new/screens/register_screen.dart';
 import 'package:ientrada_new/screens/verify_screen.dart';
-import 'package:ientrada_new/widgets/appbar.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -13,31 +18,69 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   int _currentIndex = 0;
   final List<Widget> _screens = [
+    EmployeeScreen(),
     RegisterScreen(),
     VerifyScreen(),
+    EntranceScreen(),
+    ExitScreen(),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyAppBar(),
       body: _screens[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
+      bottomNavigationBar: CurvedNavigationBar(
+        index: _currentIndex,
+        backgroundColor: Colors.transparent,
+        color: AppColors.primary,
+        height: 50,
+        animationCurve: Curves.easeInOut,
+        animationDuration: Duration(milliseconds: 300),
         onTap: (index) {
           setState(() {
             _currentIndex = index;
           });
         },
-        type: BottomNavigationBarType.fixed,
         items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_add),
-            label: 'Register',
+          CurvedNavigationBarItem(
+            child: Icon(
+              Icons.person,
+              size: 25,
+              color: AppColors.white,
+            ),
+            // label: 'Employee',
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.verified_user),
-            label: 'Verify',
+          CurvedNavigationBarItem(
+            child: Icon(
+              Icons.person_add,
+              size: 25,
+              color: AppColors.white,
+            ),
+            // label: 'Register',
+          ),
+          CurvedNavigationBarItem(
+            child: Icon(
+              Icons.verified_user_rounded,
+              size: 25,
+              color: AppColors.white,
+            ),
+            // label: 'Verify',
+          ),
+          CurvedNavigationBarItem(
+            child: Icon(
+              Icons.inbox_rounded,
+              size: 25,
+              color: AppColors.white,
+            ),
+            // label: 'Entrance',
+          ),
+          CurvedNavigationBarItem(
+            child: Icon(
+              Icons.outbox_rounded,
+              size: 25,
+              color: AppColors.white,
+            ),
+            // label: 'Exit',
           ),
         ],
       ),
